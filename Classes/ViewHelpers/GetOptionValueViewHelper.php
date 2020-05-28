6e3c7cb01e827ffac86c3b86710a986c9ed9ee20
Armin Vieweg <armin@v.ieweg.de>
2020-05-28 18:47:59 +0200

    [TASK] Add GetOptionValue ViewHelper
    Which allows you to get the OptionValue by vote and option.
    Also iterate via poll.options, not over vote.optionValues.
    And move "full" label to VotingBox partial.

---
+++ Classes/ViewHelpers/GetOptionValueViewHelper.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+namespace T3\T3oodle\ViewHelpers;
+
+use T3\T3oodle\Domain\Model\Option;
+use T3\T3oodle\Domain\Model\Vote;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class GetOptionValueViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    public function initializeArguments()
+    {
+        $this->registerArgument('vote', 'object', 'Vote object', false);
+        $this->registerArgument('option', 'object', 'Option object', true);
+    }
+
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        /** @var Vote $vote */
+        $vote = $arguments['vote'] ?? $renderChildrenClosure();
+        /** @var Option $option */
+        $option = $arguments['option'];
+
+        foreach ($vote->getOptionValues() as $key => $optionValue)  {
+            if ($optionValue->getOption() === $option) {
+                return $optionValue;
+            }
+        }
+
+        $optionValue = new \T3\T3oodle\Domain\Model\OptionValue();
+        $optionValue->setOption($option);
+        $optionValue->setValue('0');
+        return $optionValue;
+    }
+}

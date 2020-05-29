16b8cf771d977bff149473b9ac897f0c237b56bc
Armin Vieweg <armin@v.ieweg.de>
2020-05-29 13:25:03 +0200

    [FEATURE] Remember entered username/mail in LocalStorage

--- Resources/Public/JavaScript/t3oodle.js
+++ Resources/Public/JavaScript/t3oodle.js
@@ -213,4 +213,28 @@ window.onload = function() {
       }
     });
   }
+
+  // Init localstorage for
+  if (localStorage) {
+    var rememberInputs = document.querySelectorAll('*[data-remember]');
+    for (var i = 0; i < rememberInputs.length; i++) {
+      var rememberInput = rememberInputs[i];
+      if (!rememberInput.value) {
+        var key = 't3oodle-remember-' + rememberInput.dataset.remember;
+        var value = localStorage.getItem(key);
+        if (value) {
+          rememberInput.value = value;
+        }
+      }
+      rememberInput.addEventListener('change', function(event) {
+        var key = 't3oodle-remember-' + event.target.dataset.remember;
+        if (event.target.value) {
+          localStorage.setItem(key, event.target.value)
+        } else {
+          localStorage.removeItem(key)
+        }
+      });
+    }
+  }
+
 };

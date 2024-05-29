cdca487da45896cc36ab7e5f6e134046937ca70b
Philipp Kuhlmay <p.kuhlmay@web-vision.de>
2024-05-29 12:24:34 +0200

    [TASK] Set version of extension to 0.10 and update typo3 dependencies

--- Documentation/Index.rst
+++ Documentation/Index.rst
@@ -25,7 +25,7 @@ t3oodle
         TYPO3 CMS, poll, doodle, vote
 
     :Copyright:
-        2020-2021
+        2020-2024
 
     :Author:
         Armin Vieweg
--- ext_emconf.php
+++ ext_emconf.php
@@ -11,14 +11,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 't3oodle',
     'description' => 'Simple poll extension for TYPO3 CMS. t3oodle allows your frontend users to create new polls and vote for existing ones.',
     'category' => 'plugin',
-    'version' => '0.9.2',
+    'version' => '0.10.0',
     'state' => 'beta',
     'author' => 'Armin Vieweg',
     'author_email' => 'info@v.ieweg.de',
     'author_company' => 'FGTCLB',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],

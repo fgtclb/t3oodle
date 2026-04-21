ce91849cebb8e3ceb7bc2956fec243150ea083e2
Markus Hofmann <m.hofmann@web-vision.de>
2026-04-21 13:25:57 +0200

    [TASK] remove strict type declaration from ext_emconf.php
    When not fitting the TER given structure, the ext_emconf is overridden
    and will not been set by the extension itself.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-/*  | The t3oodle extension is made with ❤ for TYPO3 CMS and is licensed
- *  | under GNU General Public License.
- *  |
- *  | (c) 2020-2021 Armin Vieweg <info@v.ieweg.de>
- */
-// phpcs:disable
 $EM_CONF[$_EXTKEY] = [
     'title' => 't3oodle',
     'description' => 'Simple poll extension for TYPO3 CMS. t3oodle allows your frontend users to create new polls and vote for existing ones.',

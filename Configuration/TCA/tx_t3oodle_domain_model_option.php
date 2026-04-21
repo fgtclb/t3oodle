f268420d54dcd5a223d5b4c246a71d075e60cd7e
Markus Hofmann <m.hofmann@web-vision.de>
2026-04-21 13:11:37 +0200

    [RECTOR] Run MigrateRequiredFlagRector

--- Configuration/TCA/tx_t3oodle_domain_model_option.php
+++ Configuration/TCA/tx_t3oodle_domain_model_option.php
@@ -61,7 +61,8 @@ return [
             'config' => [
                 'type' => 'input',
                 'size' => 30,
-                'eval' => 'trim,required',
+                'eval' => 'trim',
+                'required' => true,
             ],
         ],
         'creator' => [

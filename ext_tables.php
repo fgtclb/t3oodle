db706a092c982c15275860528ce9514359fb8289
Markus Hofmann <m.hofmann@web-vision.de>
2026-04-21 13:02:04 +0200

    [RECTOR] Run MoveAllowTableOnStandardPagesToTCAConfigurationRector

--- ext_tables.php
+++ ext_tables.php
@@ -6,11 +6,9 @@
  *  | (c) 2020-2021 Armin Vieweg <info@v.ieweg.de>
  */
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 
 defined('TYPO3') || die('Access denied.');
 
 (static function (): void {
-    ExtensionManagementUtility::allowTableOnStandardPages('tx_t3oodle_domain_model_poll');
-    ExtensionManagementUtility::allowTableOnStandardPages('tx_t3oodle_domain_model_option');
 })();
